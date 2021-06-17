66dc3ecac84698d4c554963eadc09acbc1d8c594
Stephen Mahon <stemahon@gmail.com>
2021-06-16 22:31:54 -0500

    trying some sdl2 for go but having some trouble

---
+++ sdl2/sdl2.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	window, err := sdl.CreateWindow("Testing SDL2", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, 800, 600, sdl.WINDOW_SHOWN)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer window.Destroy()
+
+	sdl.Delay(2000)
+
+}
